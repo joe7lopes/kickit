5b89e1fbcd9fc0c78d3b98b0de2a133b7defdfbb
Joao Oliveira <j.andre.oliveira@gmail.com>
2017-03-04 17:39:47 +0100

    added items to recycle

--- app/src/main/java/com/intotech/kickit/MainActivity.java
+++ app/src/main/java/com/intotech/kickit/MainActivity.java
@@ -1,15 +1,20 @@
 package com.intotech.kickit;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+
+import com.intotech.kickit.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends Activity {
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
